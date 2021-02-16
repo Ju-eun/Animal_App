9806c46cffa5df6ece29ec32509daaa780f2e995
younghun <kcg0602s@naver.com>
2021-02-16 17:09:37 +0900

    아니라해줘..

---
+++ app/src/main/java/animalapp/info/SignUpActivity.java
@@ -0,0 +1,22 @@
+package animalapp.info;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Build;
+import android.os.Bundle;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+        if(Build.VERSION.SDK_INT>=21){
+            getWindow().setStatusBarColor(Color.parseColor("#000000"));
+        }
+        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(0xFF000000));
+
+    }
+}
