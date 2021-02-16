5962c77f98c708e57af80481b173398d675ac27c
younghun <kcg0602s@naver.com>
2021-02-16 19:06:32 +0900

    아니야

--- app/src/main/java/animalapp/info/LoginActivity.java
+++ app/src/main/java/animalapp/info/LoginActivity.java
@@ -1,5 +1,6 @@
 package animalapp.info;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -10,5 +11,8 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
     }
 }
