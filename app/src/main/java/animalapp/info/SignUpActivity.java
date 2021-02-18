374f6231100cdf01753b8f648e2bd018ee0119e0
younghun <kcg0602s@naver.com>
2021-02-18 22:13:03 +0900

    zz

--- app/src/main/java/animalapp/info/SignUpActivity.java
+++ app/src/main/java/animalapp/info/SignUpActivity.java
@@ -1,13 +1,39 @@
 package animalapp.info;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class SignUpActivity extends AppCompatActivity {
+    private EditText sign_id_ed;
+    private EditText sign_paassword_ed;
+
+    private Button sign_up_btn;
+
+    FirebaseAuth firebaseAuth;
+
+    DatabaseReference mRootRef = FirebaseDatabase.getInstance().getReference();
+    DatabaseReference conditionRef = mRootRef.child("email");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +42,52 @@ public class SignUpActivity extends AppCompatActivity {
         if(Build.VERSION.SDK_INT>=21){
             getWindow().setStatusBarColor(Color.parseColor("#000000"));
         }
-        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(0xFF000000));
+//        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(0xFF000000));
+
+        sign_id_ed = (EditText)findViewById(R.id.sign_id_ed);
+        sign_paassword_ed = (EditText)findViewById(R.id.sign_password_ed);
+        sign_up_btn = (Button)findViewById(R.id.sign_up_btn);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        conditionRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String email = snapshot.getValue(String.class);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        sign_up_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = sign_id_ed.getText().toString().trim();
+                String pwd = sign_paassword_ed.getText().toString().trim();
+
+                firebaseAuth.createUserWithEmailAndPassword(email,pwd)
+                        .addOnCompleteListener(SignUpActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                                if(task.isSuccessful()){
+                                    Intent intent = new Intent(SignUpActivity.this,MainActivity.class);
+                                    startActivity(intent);
+                                    finish();
+                                }else {
+                                    Toast.makeText(SignUpActivity.this,"등록 에러",Toast.LENGTH_SHORT).show();
+                                    return;
+                                }
+                            }
+                        });
+
+                conditionRef.setValue(sign_id_ed.getText().toString());
+            }
+        });
 
     }
 }
---
+++ app/src/main/java/animalapp/info/User.java
@@ -0,0 +1,69 @@
+package animalapp.info;
+
+public class User {
+    private String id;
+    private String pwd;
+    private String name;
+    private String phone;
+    private String pet_name;
+    private String pet_type;
+    private String pet_gender;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPet_name() {
+        return pet_name;
+    }
+
+    public void setPet_name(String pet_name) {
+        this.pet_name = pet_name;
+    }
+
+    public String getPet_type() {
+        return pet_type;
+    }
+
+    public void setPet_type(String pet_type) {
+        this.pet_type = pet_type;
+    }
+
+    public String getPet_gender() {
+        return pet_gender;
+    }
+
+    public void setPet_gender(String pet_gender) {
+        this.pet_gender = pet_gender;
+    }
+
+    public User(){}
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
+}
